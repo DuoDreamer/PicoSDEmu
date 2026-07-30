1afa9bce1275f9f26f0501964422157d6368d42e
Duo <rownik@hotmail.com>
2026-07-29 20:48:02 -0700

    Expose CDC remote error codes

---
+++ shared/protocol/include/picosd/protocol/version.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace picosd::protocol {
+
+inline constexpr std::uint16_t kVersionMajor = 0;
+inline constexpr std::uint16_t kVersionMinor = 1;
+
+}  // namespace picosd::protocol
+
